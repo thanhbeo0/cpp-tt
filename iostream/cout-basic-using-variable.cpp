dca162d22cb9cf0c3dd50eaabd78e488b9207816
thanhbeo0 <lyvanthan16049@gmail.com>
2025-07-01 12:06:11 +0700

    Create cout-basic-using-variable.cpp

---
+++ iostream/cout-basic-using-variable.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+// Still import iostream header
+
+int main(int argc, char* argv[])
+{
+  int x = 3; //Declare variable x of data type int
+  //cout prints out variable
+  std::cout << x << std::endl; // print x
+  //Output:3
+
+  //cout prints the result of addition
+  std::cout << "x + 5 = " << x + 5 << std::endl;
+  //Output:x + 5 = 8
+  return 0;
+}
